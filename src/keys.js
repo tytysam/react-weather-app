375f1473a6203ca2347bd1e5acde89e29e7cbf4f
Tyler Samuelson <tylercsamuelson@gmail.com>
2020-12-29 11:38:09 -0700

    solving mixed content errors

--- src/keys.js
+++ src/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
   API_KEY: "6bec8a4508623007a5a5a0da7d34a39d",
-  BASE_URL: "http://api.openweathermap.org/data/2.5/",
+  BASE_URL: "https://api.openweathermap.org/data/2.5/",
 };
